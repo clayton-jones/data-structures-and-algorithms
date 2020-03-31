c3ade05973fda2dc83a57f57990f5aa56febb37f
Clayton Jones <claytonjonesLMP@gmail.com>
2020-03-31 15:37:00 -0700

    methods seem to work as intended, need to write tests

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -74,9 +74,90 @@ class LinkedList {
     console.log(str);
     return str;
   }
+
+  append(value) {
+    let currentNode = this.head;
+
+    // loops until currentNode.next doesn't exist
+    // meaning we've reached the end of the list
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    let newNode = new Node(value);
+    currentNode.next = newNode;
+
+    return newNode;
+  }
+
+  insertBefore(listValue, newValue) {
+    let currentNode = this.head;
+
+    // if the value being search for is the first node in the list
+    if (currentNode.val === listValue) {
+      let newNode = new Node(newValue);
+      newNode.next = currentNode;
+      this.head = newNode;
+  
+      return newNode;
+    }
+
+    // otherwise:
+  
+    // loops until the value of the node after the current is the listValue
+    // being searched for, if we get to a null next node, the value doesn't exist
+    while (currentNode.next.val !== listValue) {
+      currentNode = currentNode.next;
+
+      if (!currentNode.next) {
+        console.error('ERROR! No Node with given value found in list.');
+        return false;
+      }
+    }
+    
+    let newNode = new Node(newValue);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+
+    return newNode;
+  }
+
+  insertAfter(listValue, newValue) {
+    let currentNode = this.head;
+
+    while (currentNode && currentNode.val !== listValue) {
+      currentNode = currentNode.next;
+    }
+
+    if (!currentNode) {
+      console.error('ERROR! No Node with given value found in list.');
+      return false;
+    }
+
+    let newNode = new Node(newValue);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+
+    return newNode;
+  }
 }
 
+let newLL = new LinkedList();
 
+newLL.insert('A');
+
+newLL.toString();
+
+newLL.append('B');
+
+newLL.toString();
+
+newLL.append('C');
+
+newLL.toString();
+
+newLL.insertBefore('A', 'Before A');
+
+newLL.toString();
 
 module.exports = { LinkedList, Node };
 
