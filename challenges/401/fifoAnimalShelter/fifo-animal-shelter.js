2730e9ac56c90e662a2e22ee811c152dd82af1e2
Clayton Jones <claytonjonesLMP@gmail.com>
2020-04-23 16:41:17 -0700

    shelter methods finished, testing complete

--- challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
@@ -16,21 +16,64 @@ class AnimalShelter {
 
   // is animal an object or a string used to make an object?
   enqueue(animal) {
+    if( animal.toLowerCase() !== 'cat' && animal.toLowerCase() !== 'dog' ) {
+      throw Error('Not a valid animal');
+    }
 
-    if( animal.species !== 'cat' || animal.species !== 'dog' ) return Error('Not a valid animal');
+    let animalNode = new Animal(animal);
 
-    if (this.isEmpty()) {
-      this.front = animal;
-      this.rear = animal;
+    if (!this.front) {
+      this.front = animalNode;
+      this.rear = animalNode;
       return;
     } 
 
-    this.rear.next = animal;
-    this.rear = animal;
+    this.rear.next = animalNode;
+    this.rear = animalNode;
     return;
   }
 
-  dequeue(animal) {
+  dequeue(pref) {
+    if (!this.front) {
+      throw Error('Shelter is empty, hurray!');
+    }
 
+    if( pref.toLowerCase() !== 'cat' && pref.toLowerCase() !== 'dog' ) {
+      return null;
+    }
+
+    let prevAnimal = null;
+    let currentAnimal = this.front;
+
+    while(currentAnimal) {
+      if (currentAnimal.species === pref.toLowerCase()) {
+        if (!currentAnimal.next) {
+          this.rear = prevAnimal;
+          prevAnimal.next = null;
+        }
+        
+        if (!prevAnimal) {
+          this.front = currentAnimal.next;
+        } else {
+          prevAnimal.next = currentAnimal.next;
+        }
+        currentAnimal.next = null;
+        
+        return currentAnimal.species;
+      }
+
+      prevAnimal = currentAnimal;
+      currentAnimal = currentAnimal.next;
+    }
+
+    let otherAnimal;
+
+    if (pref.toLowerCase() === 'cat') otherAnimal = 'dog';
+    else otherAnimal = 'cat';
+
+    return `No animals are left in the shelter fitting your preference. Would you like a ${otherAnimal} instead?`;
   }
 }
+
+
+module.exports = AnimalShelter;
